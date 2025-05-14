f5e7a8b15bd7314465a96ffd2a7a6c8ae3fd8a15
Smaran Yanapu <smaranyanapu@gmail.com>
2025-05-14 22:12:11 +0530

    Create 06_Rotate_Array_By_k_Elements.cpp

---
+++ Striver's A2Z DSA Sheet/02 Arrays/Easy/06_Rotate_Array_By_k_Elements.cpp
@@ -0,0 +1,38 @@
+// Given an integer array nums, rotate the array to the left by k steps, where k is non-negative.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> copy(nums);
+        int n = nums.size();
+        k = k % n; // Handle cases where k is greater than n
+        for(int i = 0; i < n; i++) {
+            nums[i] = copy[(i + k) % n];
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+
+    cout << "Original array: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    solution.rotate(nums, k);
+
+    cout << "Rotated array: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
