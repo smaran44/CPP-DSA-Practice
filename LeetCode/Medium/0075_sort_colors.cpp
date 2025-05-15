b6e6b03fdd40bad54d40e63992b85fdb634abebb
Smaran Yanapu <smaranyanapu@gmail.com>
2025-05-15 22:54:40 +0530

    create Sort Colors

---
+++ LeetCode/Medium/0075_sort_colors.cpp
@@ -0,0 +1,42 @@
+// Sort Colors
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+// You must solve this problem without using the library's sort function.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums[j], nums[j + 1]);
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+    
+    cout << "Before sorting: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    solution.sortColors(nums);
+
+    cout << "After sorting: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
