164ecb70434465215597a199c3310f0f684d4b8f
Smaran Yanapu <smaranyanapu@gmail.com>
2025-05-16 18:02:06 +0530

    create sort colors

--- Striver's A2Z DSA Sheet/02 Arrays/Medium/02_sort_colors.cpp
+++ Striver's A2Z DSA Sheet/02 Arrays/Medium/02_sort_colors.cpp
@@ -6,19 +6,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n = nums.size();
+//         for (int i = 0; i < n - 1; i++) {
+//             for (int j = 0; j < n - i - 1; j++) {
+//                 if (nums[j] > nums[j + 1]) {
+//                     swap(nums[j], nums[j + 1]);
+//                 }
+//             }
+//         }
+//     }
+// };
+// O(n^2) solution using bubble sort
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    swap(nums[j], nums[j + 1]);
-                }
+        int low = 0, mid = 0, high = nums.size() - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high]);
+                high--;
             }
         }
     }
 };
+// O(n) solution using Dutch National Flag algorithm
+// This algorithm sorts the array in a single pass with O(1) space complexity.
 
 int main() {
     Solution solution;
